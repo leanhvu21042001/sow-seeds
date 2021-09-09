54680a84d5bcca3994af6e3a533589219b904ee1
leanhvu21042001@gmail.com <leanhvu1412>
2021-09-09 20:19:56 +0700

    Chúc năng background âm nhạc

--- game_lam_vuon/src/components/SettingSection.js
+++ game_lam_vuon/src/components/SettingSection.js
@@ -1,18 +1,67 @@
-
+import { useEffect, useState } from "react";
+import { backgroundMusics } from "./../services/sounds.service";
 function SettingSection() {
-    
+
+    const [bgSound] = useState(new Audio(backgroundMusics.Vexento));
+    const [isPlayOrPause, setIsPlayOrPause] = useState(false);
+    const [isStop, setIsStop] = useState(false);
+    useEffect(() => {
+        if (isPlayOrPause === true) {
+            bgSound.volume = 0.25;
+            bgSound.loop = true;
+            bgSound.play();
+            setIsStop(false);
+        } else {
+            bgSound.pause();
+        }
+    }, [isPlayOrPause]);
+
+    useEffect(() => {
+        if (isStop === true) {
+            bgSound.pause();
+            bgSound.currentTime = 0;
+            setIsPlayOrPause(false);
+        }
+    }, [isStop]);
+
+    const handleChangeVolume = (event) => {
+        bgSound.volume = event.target.value / 100;
+    }
+
     return (
         <div className="setting-section">
             <div className="bgmSetting-section">
                 <div className="bgmSetting-title">Background music:</div>
                 <div className="bgmSetting-controls">
                     <div className="bgmSetting-btns">
-                        <div className="bgmSetting-btn" id="btn-PlayPause_bgMusic">Play</div>
-                        <div className="bgmSetting-btn" id="btn-Stop_bgMusic">Stop</div>
+                        <div
+                            role="button"
+                            onClick={() => {
+                                setIsPlayOrPause(!isPlayOrPause);
+                            }}
+                            className="bgmSetting-btn" id="btn-PlayPause_bgMusic">
+                            {isPlayOrPause ? 'Pause' : 'Play'}
+                        </div>
+                        <div
+                            role="button"
+                            onClick={() => {
+                                setIsStop(!isStop);
+                            }}
+                            className="bgmSetting-btn"
+                            id="btn-Stop_bgMusic">
+                            Stop
+                        </div>
                     </div>
                     <div className="bgmSetting-slider">
                         <div className="slider-name">Volume:</div>
-                        <input type="range" name="bgMusicVolume-input" id="bgMusicVolume-input" className="volume-slider" min="0" max="100" defaultValue="100"></input>
+
+                        <input
+                            name="bgMusicVolume-input" id="bgMusicVolume-input" className="volume-slider"
+                            min={0}
+                            max={100}
+                            defaultValue={25}
+                            onChange={(event) => { handleChangeVolume(event) }}
+                            type="range" />
                     </div>
                 </div>
             </div>
---
+++ game_lam_vuon/src/services/sounds.service.js
@@ -0,0 +1,9 @@
+import Vexento from '../assets/audios/bgm/Vexento-Spark.mp3';
+import LordeYellowFlickerBeat from '../assets/audios/bgm/Lorde-YellowFlickerBeat.mp3';
+import pop_1 from '../assets/audios/sounds/pop_1.mp3';
+import pop_2 from '../assets/audios/sounds/pop_2.mp3';
+
+const backgroundMusics = { Vexento, LordeYellowFlickerBeat }
+const singleSound = { pop_1, pop_2 }
+
+export { backgroundMusics, singleSound };
