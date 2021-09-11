7c6a15ddb60aaf2128284727adf7a0e4ef192db7
Linh Nguyen <nguyenphuclinh3010@gmail.com>
2021-09-11 14:48:12 +0700

    Hoan thanh sua am thanh

---
+++ game_lam_vuon/src/services/sound.service.js
@@ -0,0 +1,9 @@
+import Vexento_Spark from '../assets/audios/bgm/Vexento-Spark.mp3';
+import Lorde_YellowFlickerBeat from '../assets/audios/bgm/Lorde-YellowFlickerBeat.mp3';
+import pop_1 from '../assets/audios/sounds/pop_1.mp3';
+import pop_2 from '../assets/audios/sounds/pop_2.mp3';
+
+const backgroundMusic = { Vexento_Spark, Lorde_YellowFlickerBeat }
+const sound = { pop_1, pop_2 }
+
+export { backgroundMusic, sound };
